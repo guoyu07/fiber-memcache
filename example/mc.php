6a3ca0f2adbd862ffb6578ad796ad999549ec3cf
吕海涛 <i@lvht.net>
2017-11-25 17:54:01 +0800

    使用once方法简化示例

--- example/mc.php
+++ example/mc.php
@@ -1,17 +1,9 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-use Amp\Loop;
 use Fiber\Helper as f;
 
-Loop::run(function () {
-    $f = new Fiber(function () {mc(); });
-
-    f\run($f);
-});
-
-function mc()
-{
+f\once(function () {
     $db = new \Fiber\Memcache\Connection('127.0.0.1');
     var_dump($db->add('bar', 123));
-}
+});
